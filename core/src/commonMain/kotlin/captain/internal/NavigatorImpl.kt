4bb9abf351c0d61b134c70dfdfec561cfd513558
andylamax <andylamax@programmer.net>
2023-05-08 19:44:34 +0300

    Made paginator loader configurable even after initialization

--- core/src/commonMain/kotlin/captain/internal/NavigatorImpl.kt
+++ core/src/commonMain/kotlin/captain/internal/NavigatorImpl.kt
@@ -1,11 +1,11 @@
 package captain.internal
 
 import captain.Navigator
-import cinematic.mutableLiveOf
+import cinematic.singleWatchableLiveOf
 
 @PublishedApi
 internal class NavigatorImpl(private val root: String) : Navigator {
-    override val destination = mutableLiveOf(root)
+    override val destination = singleWatchableLiveOf(root)
     override fun navigate(path: String) {
         destination.value = path
     }
