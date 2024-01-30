cc5784ad9a2dd9f2afeefd18edba2e9f7990b603
andylamax <andylamax@programmer.net>
2024-01-31 01:44:32 +0300

    Removed DSL scope violation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import com.vanniktech.maven.publish.SonatypeHost
 
-@Suppress("DSL_SCOPE_VIOLATION") plugins {
+plugins {
     alias(androidx.plugins.library) apply false
     alias(kotlinz.plugins.multiplatform) apply false
     alias(kotlinz.plugins.serialization) apply false
