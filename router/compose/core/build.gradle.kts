1a723f87a87a5fe4d38728e2353379215c3d14aa
andylamax <andylamax@programmer.net>
2024-08-14 02:40:27 +0300

    Added the wasm targets as well

--- router/compose/core/build.gradle.kts
+++ router/compose/core/build.gradle.kts
@@ -1,7 +1,6 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import kotlin.apply
 
 plugins {
     kotlin("multiplatform")
@@ -12,17 +11,10 @@ plugins {
 description = "A compose router that follows any type of navigator"
 
 kotlin {
-//    jvm { library() }
-//    if (Targeting.JS) js(IR) { library() }
-////    if (Targeting.WASM) wasm { library() }
-//    val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
-////    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
-//    val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
-////    val mingwTargets = if (Targeting.MINGW) mingwTargets() else listOf()
 
     if (Targeting.JVM) jvm { library() }
     if (Targeting.JS) js(IR) { library() }
-//    if (Targeting.WASM) wasm { library() }
+    if (Targeting.WASM) wasmJs { library() }
 
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
 //    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
@@ -46,26 +38,16 @@ kotlin {
     }
 }
 
-compose {
-    kotlinCompilerPlugin.set(kotlinz.versions.compose.compiler)
-
-    desktop {
-        application {
-            mainClass = "MainKt"
-            nativeDistributions {
-                targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-                packageName = "captain-demo"
-                packageVersion = "1.0.0"
-            }
-        }
-    }
+rootProject.the<NodeJsRootExtension>().apply {
+    version = npm.versions.node.version.get()
+    downloadBaseUrl = npm.versions.node.url.get()
 }
 
-tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinCompile::class).configureEach {
-    kotlinOptions {
-        val v = kotlinz.versions.kotlin.get()
-        freeCompilerArgs += listOf(
-            "-P", "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=$v"
-        )
-    }
+rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
+    args.add("--ignore-engines")
+}
+
+tasks.named("wasmJsTestTestDevelopmentExecutableCompileSync").configure {
+    mustRunAfter(tasks.named("jsBrowserTest"))
+    mustRunAfter(tasks.named("jsNodeTest"))
 }
--- router/core/build.gradle.kts
+++ router/core/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import kotlin.apply
+
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
@@ -9,7 +13,8 @@ description = "An abstract router that can be used anywhere"
 kotlin {
     jvm { library() }
     if (Targeting.JS) js(IR) { library() }
-//    if (Targeting.WASM) wasm { library() }
+    if (Targeting.WASM) wasmJs { library() }
+    if (Targeting.WASM) wasmWasi { library() }
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
 //    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
@@ -30,4 +35,18 @@ kotlin {
             }
         }
     }
+}
+
+rootProject.the<NodeJsRootExtension>().apply {
+    version = npm.versions.node.version.get()
+    downloadBaseUrl = npm.versions.node.url.get()
+}
+
+rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
+    args.add("--ignore-engines")
+}
+
+tasks.named("wasmJsTestTestDevelopmentExecutableCompileSync").configure {
+    mustRunAfter(tasks.named("jsBrowserTest"))
+    mustRunAfter(tasks.named("jsNodeTest"))
 }
